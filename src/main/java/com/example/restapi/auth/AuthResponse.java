6a4ff3f1e886d2b28766272f33e0dc274ad6bbfc
1DangQuang1 <quangci112@gmail.com>
2025-08-17 19:56:51 +0700

    authen api

---
+++ src/main/java/com/example/restapi/auth/AuthResponse.java
@@ -0,0 +1,16 @@
+package com.example.restapi.auth;
+
+public class AuthResponse {
+    private String access_token;
+    private User user;
+
+    public AuthResponse(String access_token, User user) {
+        this.access_token = access_token;
+        this.user = user;
+    }
+
+    public String getAccess_token() { return access_token; }
+    public void setAccess_token(String access_token) { this.access_token = access_token; }
+    public User getUser() { return user; }
+    public void setUser(User user) { this.user = user; }
+}
