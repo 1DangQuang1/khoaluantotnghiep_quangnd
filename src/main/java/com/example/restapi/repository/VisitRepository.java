ded7fc7ba28a0d3258bfa7d1b385085f75076636
@quangnd <quangnd@mhsolution.vn>
2025-09-13 15:46:53 +0700

    Update

--- src/main/java/com/example/restapi/repository/VisitRepository.java
+++ src/main/java/com/example/restapi/repository/VisitRepository.java
@@ -15,11 +15,11 @@ public interface VisitRepository extends JpaRepository<Visit, Long> {
     int countByDepartmentIdAndVisitDateAndShift(Long departmentId, LocalDate visitDate, Integer shift);
     Optional<Visit> findTopByPatientIdOrderByCreatedAtDesc(Long patientId);
 
-    @Query(value ="""
+    @Query("""
     SELECT v FROM Visit v
     WHERE (:status IS NULL OR v.status = :status)
       AND (:departmentId IS NULL OR v.departmentId = :departmentId)
-    """, nativeQuery= true)
+    """)
     List<Visit> findByFilters(@Param("status") Visit.VisitStatus status,
                             @Param("departmentId") Long departmentId);
 
