05ba884bb32161e11ef67f0f2a66cc03584181ba
@quangnd <quangnd@mhsolution.vn>
2025-09-16 05:05:04 +0700

    Resolve query duplicate data error

--- src/main/java/com/example/restapi/repository/PatientRepository.java
+++ src/main/java/com/example/restapi/repository/PatientRepository.java
@@ -13,27 +13,32 @@ import com.example.restapi.model.Patient;
 public interface PatientRepository extends JpaRepository<Patient, Long>,  JpaSpecificationExecutor<Patient> {
     Optional<Patient> findByCccd(String cccd);
     @Query(value = """
-        WITH base AS (
-            SELECT ev.patient_id, dm.department_name, ev.visit_date, ev.status,
-                COUNT(*) OVER (PARTITION BY ev.patient_id) AS visit_count
+        WITH ranked AS (
+            SELECT ev.patient_id,
+                dm.department_name,
+                ev.status,
+                ev.visit_date,
+                COUNT(*) OVER (PARTITION BY ev.patient_id) AS visit_count,
+                ROW_NUMBER() OVER (PARTITION BY ev.patient_id ORDER BY ev.visit_date DESC) AS rn
             FROM ehrm.emr_visits ev
             LEFT JOIN ehrm.emr_department_mapping dm ON ev.department_id = dm.department_id
             WHERE ev.patient_id = :patientId
-        ), nx_base AS (
-            SELECT patient_id, department_name, MAX(visit_date) AS visit_date, status, visit_count
-            FROM base
-            GROUP BY patient_id, department_name, status, visit_count
         )
-        SELECT ep.id, ep.full_name, ep.cccd , ep.gender ,
+        SELECT ep.id,
+            ep.full_name,
+            ep.cccd,
+            ep.gender,
             EXTRACT(YEAR FROM age(ep.birth_date)) AS age,
             ep.insurance_number,
-            ep.phone, ep.address ,
-            n.department_name,
-            n.status,
-            n.visit_date AS last_visit_date,
-            n.visit_count
+            ep.phone,
+            ep.address,
+            r.department_name,
+            r.status,
+            r.visit_date AS last_visit_date,
+            r.visit_count
         FROM ehrm.emr_patients ep
-        JOIN nx_base n ON ep.id = n.patient_id
+        JOIN ranked r ON ep.id = r.patient_id
+        WHERE r.rn = 1;
     """, nativeQuery = true)
     Optional<LatestVisitInfo> findLatestVisitInfoByPatientId(@Param("patientId") Long patientId);
 }
